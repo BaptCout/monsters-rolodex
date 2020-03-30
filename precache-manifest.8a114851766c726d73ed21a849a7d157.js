39294e9c034867dac12ee81979a19ef375131b7f
Baptiste Coutard <baptiste.coutard@havasdigitalfactory.com>
2020-03-30 10:00:06 +0200

    Updates

---
+++ precache-manifest.8a114851766c726d73ed21a849a7d157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8ad0cd2a5e3b0f135c085d43fd8a35",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "33154129b7ee0fa87d58",
+    "url": "/monsters-rolodex/static/css/main.31e89f44.chunk.css"
+  },
+  {
+    "revision": "2dcba8f4d9675eb0f4a8",
+    "url": "/monsters-rolodex/static/js/2.c8b6800c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.c8b6800c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33154129b7ee0fa87d58",
+    "url": "/monsters-rolodex/static/js/main.a49cb820.chunk.js"
+  },
+  {
+    "revision": "5018ed9a0afe64468450",
+    "url": "/monsters-rolodex/static/js/runtime-main.61c619d4.js"
+  }
+]);
